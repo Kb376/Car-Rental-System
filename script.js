ad164f005c0ee12606aba73f546b75f6f07ff08d
Jamison Williams <Jamisonw0302@gmail.com>
2024-11-12 18:46:26 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+function makeReservation() {
+    alert("Making reservation...");
+    // Call to reservationDetails() or displayForm() functions can go here
+}
+
+function updateReservation() {
+    alert("Updating reservation...");
+    // Update form data with modifyReservation() or getInput()
+}
+
+function cancelReservation() {
+    alert("Cancelling reservation...");
+    // Trigger reservation cancelation flow
+}
+
+function logReturn() {
+    alert("Logging vehicle return...");
+    // Call to logReturn() and updateStatus()
+}
+
+function inspectCar() {
+    alert("Inspecting vehicle...");
+    // Call to inspectCar() and add any condition checking here
+}
+
+function generateInvoice() {
+    alert("Generating invoice...");
+    // Generate and display invoice details here
+}
+
+function settleInvoice() {
+    alert("Settling invoice...");
+    // Settle invoice or call confirmTransaction()
+}
+
+function confirmPayment() {
+    alert("Confirming payment...");
+    // Payment process logic can be implemented here
+}
